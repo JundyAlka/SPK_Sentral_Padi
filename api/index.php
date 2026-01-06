2b802692a31243dc257b76c92dc84bf089c3374a
Jundy Alka <jundy.alkr@gmail.com>
2026-01-07 00:26:49 +0700

    Fix: Explicitly load APP_KEY from env in entry point

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Forward to the Laravel public index
+// Force config cache clear effect
+$_ENV['APP_KEY'] = getenv('APP_KEY');
+$_ENV['APP_DEBUG'] = getenv('APP_DEBUG');
+
 require __DIR__ . '/../public/index.php';
